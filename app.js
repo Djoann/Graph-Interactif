8117b280e853ba14181189f76628e662b54904fe
Fal Djoann <djoann.fal@gmail.com>
2013-09-30 00:46:09 +0200

    Click event handler working
    Click event handler working

--- app.js
+++ app.js
@@ -20,7 +20,31 @@ var query = $(".remquery").val();
     console.log("vide");
     } else {
     
-    
+    //myArray.splice(0, 2)
     
     } // end if
 } // end function
+
+function helloim(nde) {
+    console.log("heloim", nde)
+}
+
+
+
+
+function addLnk() {
+        var onequery = $(".linkqueryfrst").val();
+        var secquery = $(".linkquerysecd").val();
+    
+        if (onequery.length == 0) {
+        console.log("vide");
+        } else if (secquery.length == 0) {
+        console.log("vide");
+        } else {
+        console.log(onequery, "linked via", secquery);
+        
+        
+
+        }// end if
+
+    }
